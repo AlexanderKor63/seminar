0374181bfebb9319be883c6e96f0b815d361bc3e
Alex <alex@mail.ru>
2024-07-30 15:51:57 +0300

    base

---
+++ file1.c
@@ -0,0 +1,3 @@
+void print(int d[], int  n) {
+    for(int i=0; i<n; ++i)  printf("%d  ", d[i]);
+}
---
+++ test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void print(int d[], int  n);
+
+int main(int argc, char *argv[]) {
+	int d[] = { 1, -5, 4, 3, 10, 11, -7, 9, 8, 5 };
+	int n=10, tmp, mp;
+	
+	for(int j=0; j<n; ++j) {
+		mp=d[0];
+		for(int i=1; i<n-j; ++i) {
+			if( d[i]>mp )		mp = d[i];
+			else {
+				tmp  = d[i];
+				d[i] = d[i-1];
+				d[i-1]= tmp;
+			}
+		}
+	}
+	print(d,n);
+}
